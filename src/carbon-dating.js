ffe6e08d4dd317a10f976cc6a7f80910844c7e3c
Ali <ialimagomed@mail.ru>
2021-02-28 16:50:45 +0300

    fix: fix carbon solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(str) {
-  if (isNaN(+str) == true || +str <= 0 || str == 15.1 || str == 9000 || typeof str != 'string') return false
+  if (isNaN(+str) == true || +str <= 0 || str >= 15.1 || typeof str != 'string') return false
   return Math.ceil(Math.log((MODERN_ACTIVITY / +str)) / (0.693 / HALF_LIFE_PERIOD))
 };
